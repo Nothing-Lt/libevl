50a72b6cdb5a3cd6f240ff70dafb1525cc837698
LI NAQIN <linaqin94@gmail.com>
2023-06-11 15:01:32 +0900

    Initialial commit of libevl

---
+++ meson/copy-uapi.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+# $1 = O_UAPI
+# $2 = $prefix/includedir
+
+cd $1
+find -L evl \! \( -name '*~' \) -type f | cpio -pduvm $DESTDIR/$2/uapi/
---
+++ meson/setup-uapi.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+# Expand anything in the UAPI path meson did not (e.g. ~).
+UAPI=$(eval echo $UAPI)
+
+if test -r $UAPI/Kbuild; then
+	set \
+		$UAPI/arch/$ARCH/include/uapi/asm \
+		$UAPI/include/uapi/asm-generic \
+		$UAPI/include/uapi/evl
+else
+	set $UAPI/asm $UAPI/asm-generic $UAPI/evl
+fi
+
+for d do
+	test -d $d && continue
+	echo "meson: path given to -Duapi does not look right ($d is missing)"
+	exit 1
+done
+
+mkdir -p $O_UAPI
+for d do ln -sf $d $O_UAPI/; done
